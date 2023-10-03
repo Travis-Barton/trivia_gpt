bce949ec0224f18af568128a9fc18104adeb0abb
Travis <39603097+Travis-Barton@users.noreply.github.com>
2023-10-02 23:56:15 -0700

    args

--- welcome_to_trivia.py
+++ welcome_to_trivia.py
@@ -7,13 +7,16 @@ st.subheader('A fun AI power application that generates questions and grades ans
 col1, col2, col3 = st.columns(3)
 with col1:
     st.title("Create Questions 📝")
-    st.link_button(url='https://trivia-gpt.streamlit.app/')
+    st.link_button(label="AI based question generation",
+                   url='https://trivia-gpt.streamlit.app/')
 with col2:
     st.title("Play as Trivia Master 🎮")
-    st.link_button(url='https://trivia-gpt-gm.streamlit.app/')
+    st.link_button(label="Run a game with friends",
+        url='https://trivia-gpt-gm.streamlit.app/')
 with col3:
     st.title("Play as Participant 🎮")
-    st.link_button(url='https://trivia-gpt-participant.streamlit.app/')
+    st.link_button(label="Join a game with friends",
+        url='https://trivia-gpt-participant.streamlit.app/')
 
 
 st.markdown('Brought to you by [Travis Barton Consulting](travisbarton.com)')
