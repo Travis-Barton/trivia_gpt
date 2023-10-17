fc7a515994a87765b044ae072b66f8b82829a240
Travis Barton <travis@immortal.game>
2023-10-17 11:34:39 -0700

    new formatt for logging games!

--- game_redirect.py
+++ game_redirect.py
@@ -1,11 +1,14 @@
 import streamlit as st
+from firebase_admin import credentials, firestore
+
+
+db = firestore.client()
 
 BASE_URL = "https://trivia-gpt-participant.streamlit.app/"
 
 def validate_game_id(game_id):
-    # Placeholder function for game_id validation
-    # TODO: Add your actual validation logic here.
-    # For example, checking the game_id against a database.
+    if not db.collection(u'games').document(st.session_state.game_id).get().exists:
+        return False
     return True
 
 def main():
@@ -15,11 +18,13 @@ def main():
     user_id = st.text_input("Enter Your Team Name:")
     game_id = st.text_input("Enter Game ID:")
 
+
+
     # Validate game_id
     if user_id and game_id:
         if validate_game_id(game_id):
             redirect_url = f"{BASE_URL}?team_name={user_id}&game_id={game_id}"
-            st.markdown(f'<a href="{redirect_url}" target="_blank"><button>Join Game</button></a>', unsafe_allow_html=True)
+            st.link_button("Click here to join the game!", url=redirect_url)
         else:
             st.warning("Please enter a valid Game ID.")
 
