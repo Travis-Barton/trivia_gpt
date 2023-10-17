2e8fdf13c3fab5cbbb65f002372fed0e6b42cd85
Travis Barton <travis@immortal.game>
2023-10-17 11:50:42 -0700

    db changes

--- game_redirect.py
+++ game_redirect.py
@@ -7,19 +7,19 @@ db = get_db()
 
 BASE_URL = "https://trivia-gpt-participant.streamlit.app/"
 
+
 def validate_game_id(game_id):
-    if not db.collection(u'games').document(st.session_state.game_id).get().exists:
+    if not db.collection(u'games').document(game_id).get().exists:
         return False
     return True
+
 
 def main():
     st.title("Game Redirect")
 
     # Collect user_id and game_id from the user
-    user_id = st.text_input("Enter Your Team Name:")
-    game_id = st.text_input("Enter Game ID:")
-
-
+    user_id = st.text_input("Enter Your Team Name:", key="user_id")
+    game_id = st.text_input("Enter Game ID:", key="game_id")
 
     # Validate game_id
     if user_id and game_id:
@@ -28,6 +28,7 @@ def main():
             st.link_button("Click here to join the game!", url=redirect_url)
         else:
             st.warning("Please enter a valid Game ID.")
+
 
 if __name__ == "__main__":
     main()
