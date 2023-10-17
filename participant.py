1cc82607ef258be9f397af5f2893ba7b2b52af7e
Travis Barton <travis@immortal.game>
2023-10-17 11:08:14 -0700

    requirements

--- participant.py
+++ participant.py
@@ -2,115 +2,85 @@ import streamlit as st
 import firebase_admin
 from firebase_admin import credentials, firestore
 from datetime import datetime
-# from models.componants import Question, Answer
-# import extra_streamlit_components as stx
-from streamlitextras.cookiemanager import get_cookie_manager
 
-from models.componants import Question, Answer
+# from models.componants import Question, Answer
+
 st.set_page_config(layout="wide",
                    page_title="Lets play Trivia",
                    page_icon="🎮",
-)
+                   )
 db = firestore.client()
-cookie_manager = None
 
 
+def get_url_parameters():
+    # Getting URL parameters to set session state
+    params = st.experimental_get_query_params()
+    team_name = params.get("team_name", [None])[0]
+    game_id = params.get("game_id", [None])[0]
+    return team_name, game_id
 
-def update_user_id(user_id):
-    st.session_state.user_id = user_id
 
-
+def set_url_parameters(team_name, game_id):
+    # Updating URL parameters after setting session state
+    st.experimental_set_query_params(team_name=team_name, game_id=game_id)
 
 
 def main():
     play, settings = st.tabs(['Play', 'Settings'])
-    # st.set_page_config(layout="wide")
-    # if st.button('Fetch New Question'):
-    #     st.info("Fetching new question...")
-    #     st.experimental_rerun()
-    #
-    # # Session states
-    # if 'user_id' not in st.session_state:
-    #     st.session_state.user_id = None
-    # if 'game_id' not in st.session_state:
-    #     st.session_state.game_id = None
-    #
-    # # Join Game
-    # if not st.session_state.user_id:
-    #     st.session_state.user_id = st.text_input("Enter Your Team Name:")
-    #     if not st.session_state.user_id:
-    #         st.warning("Please enter a team name to join the game.")
-    #         st.stop()
-    # if 'game_id' not in st.session_state:
-    #     st.session_state.game_id = None
-    # if not st.session_state.game_id:
-    #     st.session_state.game_id = st.text_input("Enter Game ID:", value=None)
-    #     if not st.session_state.game_id:
-    #         st.warning("Please enter a valid Game ID to join the game.")
-    #         st.stop()
+    team_name, game_id = get_url_parameters()
 
-    global cookie_manager
-    cookie_manager = get_cookie_manager()
-    cookie_manager.delayed_init()  # Makes sure CookieManager stays in st.session_state
+    # Join Game
+    if not team_name:
+        team_name = st.text_input("Enter Your Team Name:")
+        if not team_name:
+            st.info("Please enter a team name to join the game. 🍻")
+            st.stop()
 
-    with settings:
-        # option to reset the game and start over with a new game_id and team name. Essentially nuke the cookies
-        if st.button('Reset Game'):  # delete cookies
-            cookie_manager.delete("game_id")
-            cookie_manager.delete("team_name")
-            st.rerun()
-    with play:
-        # Check if a team name is already saved in cookies
-        team_name = cookie_manager.get("team_name")
-        st.write(cookie_manager.cookies)
-        if not team_name or isinstance(team_name, bool):
-            team_name = st.text_input("Enter Your Team Name:", None)
-            if team_name:
-                # Set the team name as a cookie for 24 hours
-                cookie_manager.set("team_name", value=team_name)
+    if not game_id:
+        game_id = st.text_input("Enter Game ID:")
+        if game_id:
+            if db.collection(u'games').document(game_id).get().exists:
+                if st.button("Join Game"):
+                    set_url_parameters(team_name, game_id)
+                    st.experimental_rerun()
             else:
-                st.info("Please enter a team name to join the game. 🍻")
+                st.warning("Please enter a valid Game ID to join the game.")
                 st.stop()
         else:
-            st.markdown(f"Welcome, __{team_name}!__")
-
-        game_id = cookie_manager.get("game_id")
-        print(game_id)
-        if not game_id:
-            game_id = st.text_input("Enter Game ID:", value=None)
-            print(f"game_id: {game_id}")
-            if game_id:
-                # Check the validity of the game_id (assuming you have a function or method for that)
-                if db.collection(u'games').document(game_id).get().exists:
-                    # Save game_id as a cookie for 24 hours
-                    cookie_manager.set("game_id", game_id, key="game_id")
-                else:
-                    st.warning("Please enter a valid Game ID to join the game.")
-                    st.stop()
-        else:
-            st.markdown(f"Using Game ID: __{game_id}__")
-            # Update the session state with the game_id from the cookie
-        if 'game_id' not in st.session_state:
-            st.session_state.game_id = game_id
-        if 'user_id' not in st.session_state:
-            st.session_state.user_id = team_name
-        # if not db.collection(u'games').document(st.session_state.game_id).get().exists:
-        game_id = cookie_manager.get('game_id')
-        user_id = cookie_manager.get('user_id')
-        if not db.collection(u'games').document(game_id).get().exists:
             st.warning("Please enter a valid Game ID to join the game.")
             st.stop()
 
-        game_ref = db.collection(u'games').document(game_id)
+    with settings:
+        if st.button('Reset Game'):
+            # Clear session states and reset URL parameters
+            if 'game_id' in st.session_state:
+                del st.session_state['game_id']
+            if 'user_id' in st.session_state:
+                del st.session_state['user_id']
+            st.experimental_set_query_params()
+            st.experimental_rerun()
+
+    with play:
+        st.markdown(f"Welcome, __{team_name}!__")
+        st.markdown(f"Using Game ID: __{game_id}__")
+
+        # Update the session state with values from URL parameters
+        st.session_state.game_id = game_id
+        st.session_state.user_id = team_name
+        if not db.collection(u'games').document(st.session_state.game_id).get().exists:
+            st.warning("Please enter a valid Game ID to join the game.")
+            st.stop()
+
+        game_ref = db.collection(u'games').document(st.session_state.game_id)
         game = game_ref.get().to_dict()
         if st.session_state.user_id not in game['user_ids']:
             game_ref.update({
-                'user_ids': firestore.ArrayUnion([user_id])
+                'user_ids': firestore.ArrayUnion([st.session_state.user_id])
             })
 
         # Display Open Questions
         # Fetch all questions for the game without sorting
-        questions_ref = db.collection(u'questions').where(u'game_id', u'==', game_id).where(u'revealed',
+        questions_ref = db.collection(u'questions').where(u'game_id', u'==', st.session_state.game_id).where(u'revealed',
                                                                                                              u'==',
                                                                                                              True).stream()
         open_questions = {doc.id: doc.to_dict() for doc in questions_ref}
@@ -128,7 +98,7 @@ def main():
         st.markdown(f"**{latest_question['question']}**")
 
         # Check if user has already answered this question
-        answered_already = db.collection(u'answers').where(u'user_id', u'==', user_id).where(
+        answered_already = db.collection(u'answers').where(u'user_id', u'==', st.session_state.user_id).where(
             u'question_id', u'==', latest_question_id).get()
 
         if not answered_already:
@@ -140,8 +110,8 @@ def main():
             if st.button(f"Submit Your Final Answer", key=f"btn_{latest_question_id}"):
                 if user_answer:
                     answer_ref = db.collection(u'answers').add({
-                        'user_id': user_id,
-                        'game_id': game_id,
+                        'user_id': st.session_state.user_id,
+                        'game_id': st.session_state.game_id,
                         'question_id': latest_question_id,
                         'answer': user_answer,
                         'timestamp': datetime.now(),
@@ -157,10 +127,10 @@ def main():
 
         # list all of their answers if any if answers have been revealed
         if game['show_answers']:
-            st.markdown(f'Answers for {user_id}')
-            answers_ref = db.collection(u'answers').where(u'user_id', u'==', user_id).where(
-                u'game_id', u'==', game_id).stream()
-            questions_ref = db.collection(u'questions').where(u'game_id', u'==', game_id).stream()
+            st.markdown(f'Answers for {st.session_state.user_id}')
+            answers_ref = db.collection(u'answers').where(u'user_id', u'==', st.session_state.user_id).where(
+                u'game_id', u'==', st.session_state.game_id).stream()
+            questions_ref = db.collection(u'questions').where(u'game_id', u'==', st.session_state.game_id).stream()
             questions = {doc.id: doc.to_dict() for doc in questions_ref}
             answers = {doc.id: doc.to_dict() for doc in answers_ref}
             sorted_questions = sorted(questions.items(), key=lambda x: x[1]['order'])
